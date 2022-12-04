223aa4ce62f5cf832335cfa5b8d243e381bc968a
Zack Wins <el_hefe@mbp.attlocal.net>
2022-12-04 15:52:29 -0500

    added a custom favicon

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text } from '@mantine/core';
 import useStyles from './Welcome.styles';
 
 export function Welcome() {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
       <Head>
         <title>ASK HAPI</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
