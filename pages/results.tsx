691bc6309977c9bceb176154ed25abc4c64aefd1
Zack Wins <el_hefe@mbp.attlocal.net>
2022-12-16 12:17:36 -0500

    cleaned up results page

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { Center, Image } from '@mantine/core';
+import Link from 'next/link';
+import { Center, Container, Image } from '@mantine/core';
+import Welcome from '../components/Welcome/Welcome';
+import { IconArrowBack } from '@tabler/icons';
 
 export default function Results() {
     const router = useRouter();
@@ -14,7 +17,19 @@ export default function Results() {
             <meta name="twitter:description" content="Use AI models with no subscriptions, just micropayments" />
             <meta name="twitter:image" content={`${imgSrc}`} />
         </Head>
-        <Center> <Image title="imageFrame" src={`${imgSrc}`} /></Center>
+        <Welcome subtext="Made on AskHapi.com" />
+        <Container>
+            <Link href="/">
+            <Center>
+                <div>
+                    <Center><IconArrowBack> </IconArrowBack></Center>
+                <div>
+                <Image radius="xl" title="imageFrame" src={`${imgSrc}`} style={{ maxHeight: '480px', maxWidth: '480px' }} />
+                </div>
+                </div>
+            </Center>
+            </Link>
+        </Container>
         </>
     );
 }
